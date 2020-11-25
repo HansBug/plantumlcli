16abf472fe7ad3c4af6a79d0fd078ee6348e946a
HansBug <killog@126.com>
2020-11-26 01:13:19 +0800

    complete test for concurrent

--- test/utils/test_concurrent.py
+++ test/utils/test_concurrent.py
@@ -47,9 +47,96 @@ class TestUtilsConcurrent:
         def _timing_4():
             _func(4)
 
-        assert 1.7 < _timing_1() < 2.3
-        assert 0.7 < _timing_2() < 1.3
-        assert 0.2 < _timing_4() < 0.8
+        assert 1.6 < _timing_1() < 2.4
+        assert 0.6 < _timing_2() < 1.4
+        assert 0.1 < _timing_4() < 0.9
+
+    def test_linear_process_error_skip(self):
+        def _process(x):
+            if x == 5:
+                raise ValueError('value error for this awesome_test, value is {v}'.format(v=x))
+            else:
+                return x * x
+
+        _list = []
+        with pytest.raises(ValueError) as e:
+            linear_process(
+                items=[2, 3, 5, 7],
+                process=lambda i, x: _process(x),
+                post_process=lambda i, x, r: _list.append((i, x, r)),
+                concurrency=4,
+                skip_once_error=True,
+            )
+
+        err = e.value
+        assert isinstance(err, ValueError)
+        assert 'awesome_test' in str(err)
+        assert '5' in str(err)
+        assert _list == [(0, 2, 4), (1, 3, 9)]
+
+    def test_linear_process_error_not_skip(self):
+        def _process(x):
+            if x in [3, 7]:
+                raise ValueError('value error for this awesome_test, value is {v}'.format(v=x))
+            else:
+                return x * x
+
+        _list = []
+        with pytest.raises(ValueError) as e:
+            linear_process(
+                items=[2, 3, 5, 7],
+                process=lambda i, x: _process(x),
+                post_process=lambda i, x, r: _list.append((i, x, r)),
+                concurrency=4,
+                skip_once_error=False,
+            )
+
+        err = e.value
+        assert isinstance(err, ValueError)
+        assert 'awesome_test' in str(err)
+        assert '3' in str(err)
+        assert _list == [(0, 2, 4), (2, 5, 25)]
+
+        def _raise_last(errs):
+            raise errs[-1][2]
+
+        _list = []
+        with pytest.raises(ValueError) as e:
+            linear_process(
+                items=[2, 3, 5, 7],
+                process=lambda i, x: _process(x),
+                post_process=lambda i, x, r: _list.append((i, x, r)),
+                concurrency=4,
+                skip_once_error=False,
+                final_error_process=_raise_last,
+            )
+
+        err = e.value
+        assert isinstance(err, ValueError)
+        assert 'awesome_test' in str(err)
+        assert '7' in str(err)
+        assert _list == [(0, 2, 4), (2, 5, 25)]
+
+    def test_linear_process_error_post(self):
+        _list = []
+
+        def _post_process(i, x, r):
+            if x == 3:
+                raise ValueError('value error for this awesome_test, value is {v}'.format(v=x))
+            _list.append((i, x, r))
+
+        with pytest.raises(ValueError) as e:
+            linear_process(
+                items=[2, 3, 5, 7],
+                process=lambda i, x: x * x,
+                post_process=_post_process,
+                concurrency=4,
+            )
+
+        err = e.value
+        assert isinstance(err, ValueError)
+        assert 'awesome_test' in str(err)
+        assert '3' in str(err)
 
 
 if __name__ == "__main__":
