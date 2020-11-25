7b802a34b4caa3a3d2eaea724fad250f9309a290
HansBug <killog@126.com>
2020-11-26 00:12:51 +0800

    add test for decorator

--- test/utils/__init__.py
+++ test/utils/__init__.py
@@ -1,3 +1,4 @@
+from .test_decorator import TestUtilsDecorator
 from .test_encoding import TestUtilsEncoding
 from .test_execute import TestUtilsExecute
 from .test_file import TestUtilsFile
---
+++ test/utils/test_decorator.py
@@ -0,0 +1,53 @@
+import os
+import time
+
+import pytest
+
+from plantumlcli.utils import check_func, timing_func
+from ..test import unittest
+
+
+@unittest
+class TestUtilsDecorator:
+    def test_check_func(self):
+        @check_func(keep_return=True)
+        def func1(a: int):
+            if a < 0:
+                raise ValueError
+
+            return a + 1
+
+        assert func1(1) == (True, 2)
+        _success, _err = func1(-1)
+        assert not _success
+        assert isinstance(_err, ValueError)
+
+        @check_func(keep_return=False)
+        def func2(a: int):
+            if a < 0:
+                raise ValueError
+            return a + 1
+
+        assert func2(1)
+        assert not func2(-1)
+
+    def test_timing_func(self):
+        @timing_func(keep_return=True)
+        def func1(duration: float):
+            time.sleep(duration)
+            return duration * 2
+
+        _time, _return = func1(1.0)
+        assert 0.9 < _time < 1.1
+        assert _return == 2.0
+
+        @timing_func(keep_return=False)
+        def func2(duration: float):
+            time.sleep(duration)
+            return duration * 2
+
+        assert 0.9 < func2(1.0) < 1.1
+
+
+if __name__ == "__main__":
+    pytest.main([os.path.abspath(__file__)])
