1a25affdca5be0cb282204c38659957a48cdf5b7
HansBug <killog@126.com>
2020-11-27 00:37:02 +0800

    save it

--- env.sh
+++ env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # global configuration
-export DEMO_PATH="demo"
+if [ -z "${DEMO_PATH}" ]; then
+  export DEMO_PATH="demo"
+fi
 if [ ! -d ${DEMO_PATH} ]; then
   git clone "https://github.com/HansBug/plantumlcli-testfile.git" ${DEMO_PATH}
 fi
@@ -15,7 +17,7 @@ mkdir -p ${DEAD_PATH}
 chmod 000 ${DEAD_PATH}
 
 # demo/jar configuration
-export DEMO_JAR_PATH="demo/jar"
+export DEMO_JAR_PATH="${DEMO_PATH}/jar"
 mkdir -p ${DEMO_JAR_PATH}
 
 if [ -z "${PRIMARY_JAR_VERSION}" ]; then
