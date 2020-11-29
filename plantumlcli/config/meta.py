03d39966e4fbe23882b8e4d21e98548ac8deb024
HansBug <killog@126.com>
2020-11-29 15:35:45 +0800

    update meta info

--- plantumlcli/config/meta.py
+++ plantumlcli/config/meta.py
@@ -1,5 +1,6 @@
 # The meta information for this package
 __TITLE__ = "plantumlcli"
 __VERSION__ = "0.0.1-beta"
+__DESCRIPTION__ = 'An easy-to-use plantuml cli for everyone.'
 __AUTHOR__ = "HansBug"
 __AUTHOR_EMAIL__ = "hansbug@buaa.edu.cn"
--- setup.py
+++ setup.py
@@ -18,10 +18,16 @@ with open('requirements-test.txt', 'r', 'utf-8') as f:
     _lines = f.readlines()
     requirements_dev = [line.strip() for line in _lines if line.strip()]
 
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
 _package_version = meta['__VERSION__']
 setup(
     name=meta['__TITLE__'],
     version=_package_version,
+    description=meta['__DESCRIPTION__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
     packages=find_packages(
         include=(_package_name, "%s.*" % _package_name)
     ),
@@ -30,7 +36,24 @@ setup(
     python_requires=">=3.5",
     install_requires=requirements,
     tests_require=requirements_dev,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
     include_package_data=True,
+    project_urls={
+        'Documentation': 'https://github.com/HansBug/plantumlcli/blob/main/README.md',
+        'Source': 'https://github.com/HansBug/plantumlcli',
+    },
     entry_points={
         'console_scripts': [
             'plantumlcli=plantumlcli.entrance.cli:cli'
