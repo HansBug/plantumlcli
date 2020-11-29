0470c9214ad7c8598c7912f73af5aefacddac6d0
HansBug <killog@126.com>
2020-11-29 15:48:34 +0800

    update version

--- plantumlcli/config/meta.py
+++ plantumlcli/config/meta.py
@@ -1,6 +1,6 @@
 # The meta information for this package
 __TITLE__ = "plantumlcli"
-__VERSION__ = "0.0.1-beta"
+__VERSION__ = "0.0.1b1"
 __DESCRIPTION__ = 'An easy-to-use plantuml cli for everyone.'
 __AUTHOR__ = "HansBug"
 __AUTHOR_EMAIL__ = "hansbug@buaa.edu.cn"
