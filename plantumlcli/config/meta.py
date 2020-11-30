9bd7e7070a78afe4f21f887cc0ee51912de4ec4e
HansBug <killog@126.com>
2020-11-30 14:33:39 +0800

    bug fixed && 0.0.2 relase

--- plantumlcli/config/meta.py
+++ plantumlcli/config/meta.py
@@ -1,6 +1,6 @@
 # The meta information for this package
 __TITLE__ = "plantumlcli"
-__VERSION__ = "0.0.2b0"
+__VERSION__ = "0.0.2"
 __DESCRIPTION__ = 'An easy-to-use plantuml cli for everyone.'
 __AUTHOR__ = "HansBug"
 __AUTHOR_EMAIL__ = "hansbug@buaa.edu.cn"
